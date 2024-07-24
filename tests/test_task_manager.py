3d9ad23a3d2c068d52fcfecb75205cc4523781ee
Desmond McCarthy <desmac96@icloud.com>
2024-07-24 12:42:58 +0000

    add test for adding a task to taskmanager

---
+++ tests/test_task_manager.py
@@ -0,0 +1,16 @@
+# tests/test_task_manager.py
+
+import unittest
+from src.task_manager import TaskManager
+
+class TestTaskManager(unittest.TestCase):
+    def setUp(self):
+        self.manager = TaskManager()
+
+    def test_add_task(self):
+        self.manager.add_task("Test task")
+        self.assertEqual(len(self.manager.tasks), 1)
+        self.assertEqual(self.manager.tasks[0].description, "Test task")
+
+if __name__ == '__main__':
+    unittest.main()
