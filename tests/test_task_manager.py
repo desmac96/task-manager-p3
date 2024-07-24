e40768d4b09c13c816de6254b63fd02a30425afb
Desmond McCarthy <desmac96@icloud.com>
2024-07-24 12:50:42 +0000

    add test for removing a task from TaskManager

--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -12,5 +12,10 @@ class TestTaskManager(unittest.TestCase):
         self.assertEqual(len(self.manager.tasks), 1)
         self.assertEqual(self.manager.tasks[0].description, "Test task")
 
+    def test_remove_task(self):
+        self.manager.add_task("Test task")
+        self.manager.remove_task(0)
+        self.assertEqual(len(self.manager.tasks), 0)
+
 if __name__ == '__main__':
     unittest.main()
