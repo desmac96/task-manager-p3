5af7daf98796caf1c81ddecc5ebb2328688b6d8e
Desmond McCarthy <desmac96@icloud.com>
2024-07-24 11:00:30 +0000

    Implement remove command to accept task index

--- src/main.py
+++ src/main.py
@@ -38,3 +38,23 @@ def main():
 
     # List tasks
     list_parser = subparsers.add_parser('list', help='List all tasks')
+
+    # src/main.py
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Command-line To-Do List Application")
+    
+    subparsers = parser.add_subparsers(dest='command')
+
+    # Add task
+    add_parser = subparsers.add_parser('add', help='Add a new task')
+    add_parser.add_argument('description', type=str, help='Description of the task')
+
+    # List tasks
+    list_parser = subparsers.add_parser('list', help='List all tasks')
+
+    # Remove task
+    remove_parser = subparsers.add_parser('remove', help='Remove a task by index')
+    remove_parser.add_argument('index', type=int, help='Index of the task to remove')
