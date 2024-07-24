eef641470ad6b9baa1c598320e855ed83416a693
Desmond McCarthy <desmac96@icloud.com>
2024-07-24 10:44:26 +0000

    Implement add command to accept task description

--- src/main.py
+++ src/main.py
@@ -11,3 +11,16 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description="Command-line To-Do List Application")
     subparsers = parser.add_subparsers(dest='command')
+
+# src/main.py
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Command-line To-Do List Application")
+    
+    subparsers = parser.add_subparsers(dest='command')
+
+    # Add task
+    add_parser = subparsers.add_parser('add', help='Add a new task')
+    add_parser.add_argument('description', type=str, help='Description of the task')
