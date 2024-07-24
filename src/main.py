812432fedd2b03faafff8798cf0777c9007281df
Desmond McCarthy <desmac96@icloud.com>
2024-07-24 10:49:57 +0000

    Implement list command to list all tasks

--- src/main.py
+++ src/main.py
@@ -24,3 +24,17 @@ def main():
     # Add task
     add_parser = subparsers.add_parser('add', help='Add a new task')
     add_parser.add_argument('description', type=str, help='Description of the task')
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Command-line To-Do List Application")
+    
+    subparsers = parser.add_subparsers(dest='command')
+
+    # Add task
+    add_parser = subparsers.add_parser('add', help='Add a new task')
+    add_parser.add_argument('description', type=str, help='Description of the task')
+
+    # List tasks
+    list_parser = subparsers.add_parser('list', help='List all tasks')
