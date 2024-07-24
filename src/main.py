fc71616e32311943f191e1501e897e5c8f0f3c7b
Desmond McCarthy <desmac96@icloud.com>
2024-07-24 10:41:32 +0000

    Add subparsers for different commands

--- src/main.py
+++ src/main.py
@@ -5,3 +5,9 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description="Command-line To-Do List Application")
     args = parser.parse_args()
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Command-line To-Do List Application")
+    subparsers = parser.add_subparsers(dest='command')
